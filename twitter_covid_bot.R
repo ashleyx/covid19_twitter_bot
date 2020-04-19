edacccf55bc5a9a9980ec88ee9b130d88a08aa91
ashleyx <ashleythomasxavier@gmail.com>
2020-04-20 02:55:37 +0530

    daily update

--- twitter_covid_bot.R
+++ twitter_covid_bot.R
@@ -8,15 +8,19 @@ get_last_tweet_id <- function(){
 source("generate_plots.R")
 
 
-post_tweet(status = paste0("Covid Monitoring for India: ",Sys.Date(), "\n @theashleyxavier"),
+post_tweet(status = paste0("Covid Monitoring for India: ",Sys.Date()," \n @theashleyxavier"),
            token = token)
 
 list.files("plots", full.names = TRUE,
            pattern = Sys.Date() %>%  as.character()) %>%
     sapply(function(i){
-        post_tweet(status = gsub("^.*19|.png","",x = i) %>%
+        post_tweet(status = gsub(paste0("plots/",Sys.Date(),"_|.png"),"",x = i) %>%
                        gsub("_"," ",x=.) %>% paste0(collapse = ""),
                    token = token,
                    media = i,
                    in_reply_to_status_id = get_last_tweet_id())
-    })
+    }) %>% invisible()
+
+post_tweet(status = "Data from @covid19indiaorg API",
+           token = token,
+           in_reply_to_status_id = get_last_tweet_id())
